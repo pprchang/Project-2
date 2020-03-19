de345e32be190b86ca63b7567fcf7f2fff22b3eb
Bauter <bauter09@gmail.com>
2020-03-18 21:27:21 -0500

    slight editing to AJAX promise. Created a 'notes.md' to keep track of the details of our/my  progress

--- public/js/index.js
+++ public/js/index.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   console.log("connected");
 
   //===========================================================
-  // Import API key
+  // Import API key (stored in .env file for security)
   //===========================================================
   let keyInfo = require("public/js/keys.js");
   let apiKey = keyInfo.key
@@ -23,7 +23,7 @@ $(document).ready(function() {
     // Call function 'youtubeGO' and pass in 'searchTerm'
     youtubeGO(searchTerm);
 
-    // Post newSearch to 
+    // Post newSearch to path (middleware)
     $.post("/api/search_log", newSearch).then(function(data) {
       // log the returned data
       console.log(data);
@@ -38,33 +38,42 @@ $(document).ready(function() {
   
   function youtubeGO(searchTerm) {
 
-      // Redefine the 'searchTerm' argument passed in from callback as 'newSearch'
-      let newSearch = searchTerm;
-      // Youtube API search query (snippet, max results, order, searchterm [ q = newSearch ], & API KEY [ try to hide key ] )
-      let queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&order=relevance&q=" + newSearch + "&key=" + apiKey + "";
-  
-      // AJAX call
-      $.ajax({
-          url: queryURL,
-          method: 'GET'
-      }).then(function(response) {
+    // Redefine the 'searchTerm' argument passed in from callback as 'newSearch'
+    let newSearch = searchTerm;
 
-          // Log response in case of need to div into response object
-          console.log(response);
-          // First video in response array
-          let video1 = response.items[0].id.videoId;
+    // Youtube API search query (snippet, max results, order, searchTerm [ q = newSearch ], & API KEY [ try to hide key ] )
+    let queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&order=relevance&q=" + newSearch + "&key=" + apiKey + "";
+  
+    // AJAX call
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function(response) {
 
-          // Assign video to <iframe id="player"> (ex of iframe: '<iframe id="player" type="text/html" width="640" height="390" src="" frameborder="0" allow="encrypted-media"></iframe> )
-          $('#player').attr("src", "https://www.youtube.com/embed/" + video1 +"?enablejsapi=1");
-  
-  
-      }) // END OF 'AJAX' method / promise
+      // Log response in case of need to div into response object
+      console.log(response);
+
+      // First 5 videos in response array (make duplicates for as many returned videos as we would like)
+      let video1 = response.items[0].id.videoId;
+      let video2 = response.items[1].id.videoId;
+      let video3 = response.items[2].id.videoId;
+      let video4 = response.items[3].id.videoId;
+      let video5 = response.items[4].id.videoId;
+
+      // Assign video "src" attribute to <iframe id="player1-5"> (ex of iframe: '<iframe id="player1" type="text/html" width="640" height="390" src="" frameborder="0" allow="encrypted-media"></iframe> )
+      $('#player1').attr("src", "https://www.youtube.com/embed/" + video1 +"?enablejsapi=1");
+      $('#player2').attr("src", "https://www.youtube.com/embed/" + video2 +"?enablejsapi=1");
+      $('#player3').attr("src", "https://www.youtube.com/embed/" + video3 +"?enablejsapi=1");
+      $('#player4').attr("src", "https://www.youtube.com/embed/" + video4 +"?enablejsapi=1");
+      $('#player5').attr("src", "https://www.youtube.com/embed/" + video5 +"?enablejsapi=1");
+          
+    }) // END OF 'AJAX' method / promise
 
   }; // END OF 'youtubeGO' function
 
 
   //==========================================================
-  // Insert add. functions and event listeners below ~~~~~
+  // Insert add. functions and event listeners below VVV
   //==========================================================
   
 }); // END OF '$(document).ready' function
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -16,11 +16,13 @@ module.exports = function(app) {
     // 'Post' newSearch to log (data)
     app.post("/api/search_log", function(req, res) {
 
-        // object construction will have to be configured on html script or separate JS file
+        // Object construction will have to be configured on html script or separate JS file (location = ../public/js/index.js [ LINE 27-29] )
         let newSearch = req.body;
-    
+
+        // Console.log newSearch data in terminal
         console.log("Posting to api: " + JSON.stringify(newSearch));
 
+        // Return the newSearch data so it can be console.logged in browser
         res.json(newSearch)
 
         //When all is said and done push newSearch object to apiLog (log) array to log searches
