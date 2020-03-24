9ad6bf827ad9b808a3228c74ade58950435e2999
Bauter <bauter09@gmail.com>
2020-03-23 20:00:45 -0500

    pulling updates

--- public/js/index.js
+++ public/js/index.js
@@ -12,29 +12,36 @@ $(document).ready(function() {
     //============================================================
     // Assign 'searchTerm' the value of the search bar input.
     //============================================================
-
-    let searchTerm = $('#searchTerm').val().trim();
+    let searchTerm;
     
-    //============================================================
-    // Post newSearch to path (middleware).
-    //============================================================
+    if ($('#searchTerm').val().trim() === "") {
+      alert("You must enter a search term!")
+    } else {
+      searchTerm = $('#searchTerm').val().trim();
 
-    $.post("/api/search_log", {searchTerm}).then(function(data) {
+      //============================================================
+      // Post newSearch to path (middleware).
+      //============================================================
 
-      // log the returned data
-      console.log(data);
+      $.post("/api/search_log", {searchTerm}).then(function(data) {
 
-      //=========================================================
-      // Assign video "src" attribute to <iframe id="player1-6">.
-      //=========================================================
+        // log the returned data
+        console.log(data);
+
+        //=========================================================
+        // Assign video "src" attribute to <iframe id="player1-6">.
+        //=========================================================
       
-      $('#iFrame1').attr("src", "https://www.youtube.com/embed/" + data[0] +"?enablejsapi=1");
-      $('#iFrame2').attr("src", "https://www.youtube.com/embed/" + data[1] +"?enablejsapi=1");
-      $('#iFrame3').attr("src", "https://www.youtube.com/embed/" + data[2] +"?enablejsapi=1");
-      $('#iFrame4').attr("src", "https://www.youtube.com/embed/" + data[3] +"?enablejsapi=1");
-      $('#iFrame5').attr("src", "https://www.youtube.com/embed/" + data[4] +"?enablejsapi=1");
-      $('#iFrame6').attr("src", "https://www.youtube.com/embed/" + data[5] +"?enablejsapi=1");
-    });
+        $('#iFrame1').attr("src", "https://www.youtube.com/embed/" + data[0] +"?enablejsapi=1");
+        $('#iFrame2').attr("src", "https://www.youtube.com/embed/" + data[1] +"?enablejsapi=1");
+        $('#iFrame3').attr("src", "https://www.youtube.com/embed/" + data[2] +"?enablejsapi=1");
+        $('#iFrame4').attr("src", "https://www.youtube.com/embed/" + data[3] +"?enablejsapi=1");
+        $('#iFrame5').attr("src", "https://www.youtube.com/embed/" + data[4] +"?enablejsapi=1");
+        $('#iFrame6').attr("src", "https://www.youtube.com/embed/" + data[5] +"?enablejsapi=1");
+      });
+
+    } // END OF 'if / else' statement
+    
       
   }); // END OF 'submit' button click event
 
