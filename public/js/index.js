5c35b87023ec5781b247cfc62f9f8fc88f0199e3
Bauter <bauter09@gmail.com>
2020-03-25 14:44:20 -0500

    restoring files changed from pulling from master

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
 
+  //============================================================
+  // Global variables
+  //============================================================
+  let searchLinkCounter = 0;
+  let engineWarningCounter = 0;
+  let brakeNoiseCounter = 0;
+  let carWontStartCounter = 0;
+  let tireCounter = 0;
+  let oilCounter = 0;
+  let jumpCounter = 0;
+
   // Verify the JS file is connected to HTML via inspect
   console.log("connected");
 
@@ -12,44 +23,213 @@ $(document).ready(function() {
     //============================================================
     // Assign 'searchTerm' the value of the search bar input.
     //============================================================
-    let searchTerm;
+
+    let searchTerm = $('#searchTerm').val().trim();
     
-    if ($('#searchTerm').val().trim() === "") {
-      $('#searchTerm').addClass('is-invalid');
-    } else {
-      searchTerm = $('#searchTerm').val().trim();
-      $('#searchTerm').removeClass('is-invalid');
-      $('#searchTerm').addClass('is-valid');
+    //============================================================
+    // Post newSearch to path (middleware).
+    //============================================================
 
+    $.post("/api/search_log", {searchTerm}).then(function(data) {
 
-      //============================================================
-      // Post newSearch to path (middleware).
-      //============================================================
+      // log the returned data
+      console.log(data);
 
-      $.post("/api/search_log", {searchTerm}).then(function(data) {
-
-        // log the returned data
-        console.log(data);
-
-        //=========================================================
-        // Assign video "src" attribute to <iframe id="player1-6">.
-        //=========================================================
+      //=========================================================
+      // Assign video "src" attribute to <iframe id="player1-6">.
+      //=========================================================
       
-        $('#iFrame1').attr("src", "https://www.youtube.com/embed/" + data[0] +"?enablejsapi=1");
-        $('#iFrame2').attr("src", "https://www.youtube.com/embed/" + data[1] +"?enablejsapi=1");
-        $('#iFrame3').attr("src", "https://www.youtube.com/embed/" + data[2] +"?enablejsapi=1");
-        $('#iFrame4').attr("src", "https://www.youtube.com/embed/" + data[3] +"?enablejsapi=1");
-        $('#iFrame5').attr("src", "https://www.youtube.com/embed/" + data[4] +"?enablejsapi=1");
-        $('#iFrame6').attr("src", "https://www.youtube.com/embed/" + data[5] +"?enablejsapi=1");
-      });
-
-    } // END OF 'if / else' statement
-    
+      $('#iFrame1').attr("src", "https://www.youtube.com/embed/" + data[0] +"?enablejsapi=1");
+      $('#iFrame2').attr("src", "https://www.youtube.com/embed/" + data[1] +"?enablejsapi=1");
+      $('#iFrame3').attr("src", "https://www.youtube.com/embed/" + data[2] +"?enablejsapi=1");
+      $('#iFrame4').attr("src", "https://www.youtube.com/embed/" + data[3] +"?enablejsapi=1");
+      $('#iFrame5').attr("src", "https://www.youtube.com/embed/" + data[4] +"?enablejsapi=1");
+      $('#iFrame6').attr("src", "https://www.youtube.com/embed/" + data[5] +"?enablejsapi=1");
+    });
       
   }); // END OF 'submit' button click event
 
   //==========================================================
   // Insert add. functions and event listeners below VVV.
   //==========================================================
+
+  //==========================================================
+  // "Search" link event listener
+  //==========================================================
+
+  $("#searchLink").on("click", function() {
+    searchLinkCounter++;
+
+    console.log("search link works!") ;
+    console.log(searchLinkCounter);
+    // This is working up to this point.........
+
+    // SEND TO BACKEND
+    $.ajax({
+      method: "PUT",
+      url: "/api/data",
+      data: {searchLinkCounter}
+    }).then(function(data) {
+
+      console.log(data);
+    });
+
+  });
+
+  //==========================================================
+  // "Engine Warning Light" link event listener
+  //==========================================================
+
+  $('#engineWarning').on('click', function() {
+    engineWarningCounter++;
+
+    console.log("engine warning light listener works!");
+    console.log(engineWarningCounter);
+
+    // SEND TO BACKEND
+    $.ajax({
+      method: "PUT",
+      url: "/api/data2",
+      data: {engineWarningCounter}
+    }).then(function(data) {
+
+      console.log(data);
+    });
+    
+  });
+
+  //==========================================================
+  // "Brake Noise" link event listener
+  //==========================================================
+
+  $('#brakeNoise').on('click', function() {
+    brakeNoiseCounter++;
+
+    console.log("brake noise listener works!");
+    console.log(brakeNoiseCounter);
+
+    // SEND TO BACKEND
+    $.ajax({
+      method: "PUT",
+      url: "/api/data3",
+      data: {brakeNoiseCounter}
+    }).then(function(data) {
+
+      console.log(data);
+    });
+    
+  });
+
+  //==========================================================
+  // "Car Won't Start" link event listener
+  //==========================================================
+
+  $('#carWontStart').on('click', function() {
+    carWontStartCounter++;
+
+    console.log("car won't start listener works!");
+    console.log(carWontStartCounter);
+
+    // SEND TO BACKEND
+    $.ajax({
+      method: "PUT",
+      url: "/api/data4",
+      data: {carWontStartCounter}
+    }).then(function(data) {
+
+      console.log(data);
+    });
+    
+  });
+
+  //==========================================================
+  // "How to Change a Tire" link event listener
+  //==========================================================
+
+  $('#tire').on('click', function() {
+    tireCounter++;
+
+    console.log("tire listener works");
+    console.log(tireCounter);
+
+    // SEND TO BACKEND
+    $.ajax({
+      method: "PUT",
+      url: "/api/data5",
+      data: {tireCounter}
+    }).then(function(data) {
+
+      console.log(data);
+    });
+    
+  });
+
+  //==========================================================
+  // "How to Change Oil" link event listener
+  //==========================================================
+
+  $('#oil').on('click', function() {
+    oilCounter++;
+
+    console.log("oil listener works");
+    console.log(oilCounter);
+
+    // SEND TO BACKEND
+    $.ajax({
+      method: "PUT",
+      url: "/api/data6",
+      data: {oilCounter}
+    }).then(function(data) {
+
+      console.log(data);
+    });
+    
+  });
+
+  //==========================================================
+  // "How to Jump Start a Car" link event listener
+  //==========================================================
+
+  $('#jump').on('click', function() {
+    jumpCounter++;
+
+    console.log("jump listener works")
+    console.log(jumpCounter);
+
+    // SEND TO BACKEND
+    $.ajax({
+      method: "PUT",
+      url: "/api/data7",
+      data: {jumpCounter}
+    }).then(function(data) {
+
+      console.log(data);
+    });
+    
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
 }); // END OF '$(document).ready' function
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,55 +2,62 @@
 // Dependencies.
 //===================================================================================
 
-const path = require('path');
+const path = require("path");
 
 //===================================================================================
 // HTML Routes (as an exported module).
 //===================================================================================
 
 module.exports = function(app) {
-  // home (home.html).
-  app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public', 'home.html'));
-  });
 
-  // Search page (search.html).
-  app.get('/search', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public', 'search.html'));
-  });
+    // home (home.html).
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public", "home.html"));
+    });
 
-  // Car won't start page (carWontStart.html).
-  app.get('/car_wont_start', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public', 'carWontStart.html'));
-  });
+    // Search page (search.html).
+    app.get("/search", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public", "search.html"));
+    });
 
-  // Brake Noise page (brakeNoise.html).
-  app.get('/brake_noise', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public', 'brakeNoise.html'));
-  });
+    // Car won't start page (carWontStart.html).
+    app.get("/car_wont_start", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public", "carWontStart.html"));
+    });
 
-  // Change Oil page (changeOil.html).
-  app.get('/change_oil', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public', 'changeOil.html'));
-  });
+    // Brake Noise page (brakeNoise.html).
+    app.get("/brake_noise", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public", "brakeNoise.html"));
+    });
 
-  // Change Tire page (changeTire.html).
-  app.get('/change_tire', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public', 'changeTire.html'));
-  });
+    // Change light bulb page (changeLight.html).
+    app.get("/change_light", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public", "changeLight.html"));
+    });
 
-  // Contact page (contact.html).
-  app.get('/contact', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public', 'contactUs.html'));
-  });
+    // Change Oil page (changeOil.html).
+    app.get("/change_oil", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public", "changeOil.html"));
+    });
 
-  // Warning Lights page (engineWarning.html).
-  app.get('/warning_lights', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public', 'engineWarning.html'));
-  });
+    // Change Tire page (changeTire.html).
+    app.get("/change_tire", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public", "changeTire.html"));
+    });
 
-  // Jump-start page (jumpStart.html).
-  app.get('/jump_start', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public', 'jumpStart.html'));
-  });
+    // Contact page (contact.html).
+    app.get("/contact", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public", "contactUs.html"));
+    });
+
+    // Warning Lights page (engineWarning.html).
+    app.get("/warning_lights", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public", "engineWarning.html"));
+    });
+
+    // Jump-start page (jumpStart.html).
+    app.get("/jump_start", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public", "jumpStart.html"));
+    });
+
 };
