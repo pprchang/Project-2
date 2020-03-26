0ef4eb7fd4c20833b6bef7a9baa77484e04cefb6
Gordon Conness <gordon.conness1@gmail.com>
2020-03-26 17:40:31 -0500

    adding in js for the inputs

--- public/js/recalls.js
+++ public/js/recalls.js
@@ -1,4 +1,38 @@
 $(document).ready(function() {
 
+$("#recallSubmit").on('click', function() {
+    let model;
+    let make;
+    let year;
 
+    if ($('#model').val() === "" ) {
+        $('model').addClass("is-invalid")
+    }  else {
+        $('model').removeClass("is-invalid")
+        model = $('model').val()
+    };
+
+    if ($('#make').val() === "" ) {
+        $('make').addClass("is-invalid")
+    }  else {
+        $('make').removeClass("is-invalid")
+        make = $('make').val()
+    };
+
+    if ($('#year').val() === "" ) {
+        $('year').addClass("is-invalid")
+    }  else {
+        $('year').removeClass("is-invalid")
+        year = $('year').val()
+    };
+
+    if ($('#model').val() === "" && $('#make').val() === "" && $("#year").val() === "") {
+        console.log ("Must be filled out before moving on!")
+    } else {
+        
+
+    }
+
+
+})
 });
